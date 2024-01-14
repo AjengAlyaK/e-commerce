52ef8a326a54004b1568097ba6d134f42e2553bc
AjengAlyaK <ajenga990@gmail.com>
2024-01-15 05:10:58 +0700

    navbug

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -11,7 +11,9 @@
                 <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
                 </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true"> <span class="nav-label">Pages <span class="caret"></span></a>
+                @if (Route::has('login'))
+
+                @auth
                 <ul class="dropdown-menu">
                     <li><a href="about.html">About</a></li>
                     <li><a href="testimonial.html">Testimonial</a></li>
@@ -26,19 +28,21 @@
                 <li class="nav-item">
                 <a class="nav-link" href="/show_cart">Cart</a>
                 </li>
-
-                @if (Route::has('login'))
-
-                @auth
-                
-                <li class="nav-item">
-                    <x-app-layout>
-    
-                    </x-app-layout>                    
-                </li>
-                
                 @else
-
+                <ul class="dropdown-menu">
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="testimonial.html">Testimonial</a></li>
+                </ul>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/products">Products</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="blog_list.html">Blog</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/show_cart">Cart</a>
+                </li>
                 <li class="nav-item">
                     <a class="btn btn-dark" href="{{ route('login')}}" id="logincss">Login</a>
                 </li>
